192a0b20ccf7cbaeaaefa54c8bda8ed0590ddddb
Whxper <69385981+Whxper@users.noreply.github.com>
2022-05-04 14:08:40 +0300

    Add files via upload
    for windows

---
+++ morsechat-windows/morse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "func.h"
+
+int main(void){
+	char input[200];	
+	printf("********MorseChat********\n");
+	printf("Geben Sie den zu uebersetzenden Text ein(. eingeben um das Programm zu beenden.)\n");
+	do{	
+		scanString(input);
+		toMorse(input);
+		printf("\n");
+	}while(input[0] !='.');
+	return 0;
+}
