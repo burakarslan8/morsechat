23630442caf6ef1b11b30f41eed1fbf6b916d723
whxper <rugotr@gmail.com>
2022-05-19 13:29:02 +0300

    --help option added

--- morsechat-linux/main.c
+++ morsechat-linux/main.c
@@ -1,15 +1,20 @@
 #include "header.h"
 
 int main(int argc, char *argv[]){
-	printf("********MorseChat********\n");
-	printf("*************************\n");
+	printf("		   --_____--\n");
+	printf("		  --_______--\n");
+	printf("		 --_________--\n");
+	printf("		--|MorseChat|--\n");
+	printf("		 --¯¯¯¯¯¯¯¯¯--\n");
+	printf("		  --¯¯¯¯¯¯¯--\n");
+	printf("		   --¯¯¯¯¯--\n");
 	if(argc==3){
 
-		if(!strcmp(argv[1],"--text")){  // Den eingegebenen Text übersetzen.
+		if(!strcmp(argv[1], "--text")){  // Den eingegebenen Text übersetzen.
 			toMorse(argv[2]);
 			printf("\n");
 		}
-		else if(!strcmp(argv[1],"--file")){  // Aus einer Textdatei einlesen und übersetzen.
+		else if(!strcmp(argv[1], "--file")){  // Aus einer Textdatei einlesen und übersetzen.
 			char text[200];
 			FILE* file;
    			file = fopen(argv[2], "r");
@@ -23,6 +28,18 @@ int main(int argc, char *argv[]){
 			printf("Etwas ist schiefgegangen. Achten Sie bitte auf die Argumente.\n");
 		}
 	}
+	else if(argc==2 && !strcmp(argv[1], "--help")){	// --help Option
+			printf("Optionen:\n");
+			printf("\t--text\teinen Text eingeben und uebersetzen\n");
+			printf("\t\t    z.B: ./morsechat --text 'Hallo Welt'\n\n");
+			printf("\t--file\taus einer Textdatei einlesen und uebersetzen\n");
+			printf("\t\t    z.B: ./morsechat --file text.txt\n\n");
+			printf("\tkeine zusaetzlichen Argumente eingeben\n");
+			printf("\t\t    z.B: ./morsechat\n\n");
+			printf("\t--help\tmoegliche Optionen ansehen\n");
+			printf("\t\t    z.B: ./morsechat --help oder make help\n\n");
+	}
+	
 	else if(argc==1){			// Die eingegebenen Texte übersetzen, bis '.' eingegeben wird.(in einer Schleife)
 		char input[200];	
 		printf("Geben Sie den zu uebersetzenden Text ein(. eingeben um das Programm zu beenden.)\n");
--- morsechat-windows/main.c
+++ morsechat-windows/main.c
@@ -1,8 +1,9 @@
 #include "header.h"
 
 int main(int argc, char *argv[]){
-	printf("********MorseChat********\n");
-	printf("*************************\n");
+	printf("		    ----------------\n");
+	printf("		   -----MorseChat----\n");
+	printf("		    ----------------\n");
 	if(argc==3){
 
 		if(!strcmp(argv[1],"--text")){  // Den eingegebenen Text übersetzen.
@@ -24,6 +25,17 @@ int main(int argc, char *argv[]){
 		else{		// Der Fehlerfall
 			printf("Etwas ist schiefgegangen. Achten Sie bitte auf die Argumente.\n");
 		}
+	}
+	else if(argc==2 && !strcmp(argv[1], "--help")){	// --help Option
+			printf("Optionen:\n");
+			printf("\t--text\teinen Text eingeben und uebersetzen\n");
+			printf("\t\t    z.B: ./morsechat --text 'Hallo Welt'\n\n");
+			printf("\t--file\taus einer Textdatei einlesen und uebersetzen\n");
+			printf("\t\t    z.B: ./morsechat --file text.txt\n\n");
+			printf("\tkeine zusaetzlichen Argumente eingeben\n");
+			printf("\t\t    z.B: ./morsechat\n\n");
+			printf("\t--help\tmoegliche Optionen ansehen\n");
+			printf("\t\t    z.B: ./morsechat --help\n\n");
 	}
 	else if(argc==1){					// Die eingegebenen Texte übersetzen, bis '.' eingegeben wird.(in einer Schleife)
 		char input[200];	
